a2bb6da46b2d4a28ca829bba842be94aae6f09b8
Gunapriya S <guna37404@gmail.com>
2025-11-08 17:36:18 +0530

    Add files via upload

---
+++ divisibility/index.js
@@ -0,0 +1,13 @@
+let num = 45;
+
+if(num % 3 === 0 && num % 5 === 0){
+    console.log(num, " is divisible by 3 and 5");
+}
+else{
+    console.log(num, " is not divisible by 3 and 5");
+}
+
+//Expected Output :
+
+//45 is divisible by 3 and 5
+//27 is not divisible by 3 and 5
