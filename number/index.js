2bca113d2de0dbee90b1e73071159e6401ce0814
Gunapriya S <guna37404@gmail.com>
2025-11-08 17:20:30 +0530

    Add files via upload

---
+++ number/index.js
@@ -0,0 +1,16 @@
+let num=10
+if(num>0){
+    console.log(num,"is positive")
+}
+else if (num<0){
+    console.log(num,"is negative")
+}
+else{
+    console.log(num,"is neutral")
+}
+
+//Expected Output :
+
+//-3 => Negative
+//3 => Positive 
+//0 => Neutral
