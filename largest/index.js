1438363c8fb083b359ec62c5d97bcb7c443dcfaf
Gunapriya S <guna37404@gmail.com>
2025-11-08 17:26:51 +0530

    Add files via upload

---
+++ largest/index.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 20;
+let c = 30;
+
+if(a >= b && a >= c){
+    console.log("A is the largest number");
+}
+else if(b >= a && b >= c){
+    console.log("B is the largest number");
+}
+else{
+    console.log("C is the largest number");
+}
+
+//Expected Output :
+
+//C => 30 is the largest number
