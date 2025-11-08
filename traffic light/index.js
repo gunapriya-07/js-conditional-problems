ec0b25648651230ef42def740420b07615b0420b
Gunapriya S <guna37404@gmail.com>
2025-11-08 18:09:39 +0530

    Add files via upload

---
+++ traffic light/index.js
@@ -0,0 +1,17 @@
+let signal = "Green";
+
+if(signal === "Red"){
+    console.log("STOP");
+}
+else if(signal === "Yellow"){
+    console.log("GET READY");
+}
+else{
+    console.log("GO")
+}
+
+//Expected Output :
+
+//Red => STOP
+//Yellow => GET READY
+//Green => GO
