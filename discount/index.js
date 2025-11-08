bd5f74b41cb66f4b964fb4bf12777d22b4368866
Gunapriya S <guna37404@gmail.com>
2025-11-08 17:31:38 +0530

    Add files via upload

---
+++ discount/index.js
@@ -0,0 +1,18 @@
+let discount = 12000;
+
+if(discount >= 1000 && discount === 4000){
+    console.log("Give 10% as discount");
+}
+else if(discount >= 5000 && discount === 9000){
+    console.log("Give 15% as discount ");
+}
+else if(discount >= 10000){
+    console.log("Give 20% as discount");
+}
+else{
+    console.log("NO discount");
+}
+
+//Expected Output
+
+//Give 20% as discount
