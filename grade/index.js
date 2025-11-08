65a9f5dee23d49241dbc78042a85e16aee8cb4bd
Gunapriya S <guna37404@gmail.com>
2025-11-08 17:54:55 +0530

    Add files via upload

---
+++ grade/index.js
@@ -0,0 +1,35 @@
+let mark = 75;
+
+if(mark > 90)
+{
+    console.log("A+ Grade");
+}
+else if(mark >= 80 && mark <= 89)
+{
+    console.log("B Grade");
+}
+else if(mark >= 70 && mark <= 79)
+{
+    console.log("C Grade");
+}
+else if(mark >= 60 && mark <= 69)
+{
+    console.log("D Grade");
+}
+else if(mark >= 50 && mark <= 59)
+{
+    console.log("E Grade");
+}
+else
+{
+    console.log("F Grade");
+}
+
+//Expected Output :
+
+//95 => A+ Grade
+//82 => B Grade
+//75 => C Grade
+//63 => D Grade
+//57 => E Grade
+//40 => F Grade
